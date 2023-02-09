ac1acea594a2f6726677472b88cd689e2b3716bf
NyxTier <ultimarevolt@gmail.com>
2023-02-09 00:09:27 -0600

    Game function works in console when called

--- script.js
+++ script.js
@@ -4,49 +4,58 @@
  I need another function that has one input from the user that is case INSENSITIVE
  I need a final function that returns a string declaring a winner, a loser, or a tie */
 
- function getRandomNum () {
-    return Math.floor(Math.random() * 3)
- }
- 
- function getComputerChoice () {
-    if (getRandomNum === 0) {
-        return "PAPER"
-    } else if (getRandomNum === 1) {
-        return "ROCK"
-    } else if (getRandomNum === 2) {
-        return "SCISSORS"
-    } else {
-        return "Something went terribly wrong..."
-    }
- }
 
- function playerChoice () {
-    let choice = prompt().toUpperCase
-    return choice
- }
 
  function declareWinner () {
+    function getRandomNum () {
+        return Math.floor(Math.random() * 3)
+     }
+     let randomNum = getRandomNum()
+    
+     function getComputerChoice () {
+        getRandomNum()
+        if (randomNum === 0) {
+            return "PAPER"
+        } else if (randomNum === 1) {
+            return "ROCK"
+        } else if (randomNum === 2) {
+            return "SCISSORS"
+        } else {
+            return "Something went terribly wrong..."
+        }
+     }
+     let computerChoice = getComputerChoice()
+    
+     function playerChoice () {
+        let choice = prompt()
+        return choice
+     }
+     let playersChoice = playerChoice().toUpperCase()
     // tie
-    if (getComputerChoice === "SCISSORS" && playerChoice === "SCISSORS") {
-        console.log("It's a tie! You both chose scissors!")
-    } else if (getComputerChoice === "ROCK" && playerChoice === "ROCK") {
-        console.log("It's a tie! You both chose rock!")
-    } else if (getComputerChoice === "PAPER" && playerChoice === "PAPER") {
-        console.log("It's a tie! You both chose paper!")
+    if (computerChoice === "SCISSORS" && playersChoice === "SCISSORS") {
+        return("It's a tie! You both chose scissors!")
+    } else if (computerChoice === "ROCK" && playersChoice === "ROCK") {
+        return("It's a tie! You both chose rock!")
+    } else if (computerChoice === "PAPER" && playersChoice === "PAPER") {
+        return("It's a tie! You both chose paper!")
     // Rock wins
-    } else if (getComputerChoice === "SCISSORS" && playerChoice === "ROCK" ) {
-        console.log("Player Wins! Rock beats scissors!")
-    } else if (getComputerChoice === "ROCK" && playerChoice === "SCISSORS") {
-        console.log("PC wins! Rock beats scissors!")
+    } else if (computerChoice === "SCISSORS" && playersChoice === "ROCK" ) {
+        return("Player Wins! Rock beats scissors!")
+    } else if (computerChoice === "ROCK" && playersChoice === "SCISSORS") {
+        return("PC wins! Rock beats scissors!")
     // Scissors wins
-    } else if (getComputerChoice === "PAPER" && playerChoice === "SCISSORS") {
-        console.log("Player wins! Scissors beats paper!")
-    } else if (getComputerChoice === "SCISSORS" && playerChoice === "PAPER") {
-        console.log("PC wins! Scissors beats paper!")
+    } else if (computerChoice === "PAPER" && playersChoice === "SCISSORS") {
+        return("Player wins! Scissors beats paper!")
+    } else if (computerChoice === "SCISSORS" && playersChoice === "PAPER") {
+        return("PC wins! Scissors beats paper!")
     // Paper wins
-    } else if (getComputerChoice === "PAPER" && playerChoice === "ROCK") {
-        console.log("PC wins! Paper beats rock!")
-    } else if (getComputerChoice === "ROCK" && playerChoice === "PAPER") {
-        console.log("Player wins! Paper beats rock!")
+    } else if (computerChoice === "PAPER" && playersChoice === "ROCK") {
+        return("PC wins! Paper beats rock!")
+    } else if (computerChoice === "ROCK" && playersChoice === "PAPER") {
+        return("Player wins! Paper beats rock!")
+    // Error
+    } else {
+        console.log(randomNum + ", " + computerChoice + ", " + playersChoice)
+        return("An error occurred.")
     }
  }
